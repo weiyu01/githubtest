18929660822e7306e864c23bcf5cb1b5a9b30c44
周明涛 <zhoumingtao@zhoumingtaodeMac-mini.local>
2017-02-22 09:11:42 +0800

    p q

---
+++ githubtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubtest
+//
+//  Created by 周明涛 on 2017/2/22.
+//  Copyright © 2017年 zhou_sir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubtest
+//
+//  Created by 周明涛 on 2017/2/22.
+//  Copyright © 2017年 zhou_sir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
